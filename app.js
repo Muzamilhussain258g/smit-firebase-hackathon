b1eba6fc399e5cbcea4ee941be2f042b20eb46c3
Muzamilhussain258g <muzamilhussain258g@gmail.com>
2024-09-06 02:04:43 -0700

    make more stable
    make more stable

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 
-import { app, auth } from "./firebase.js"
+import { app, auth, db, collection, addDoc, getDocs, doc, deleteDoc } from "./firebase.js"
 // import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 // import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 // import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js"; 
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
+// target section blog start
+let blogSection = document.querySelector("#blog-section")
+let delBlogBtn = document.querySelector("#del_blog_btn")
+let editBlogBtn = document.querySelector("#edit_blog_btn")
+// target section blog end
 
 // on auth state change
 onAuthStateChanged(auth, (user) => {
@@ -25,16 +30,16 @@ onAuthStateChanged(auth, (user) => {
 
 // sign out start
 
-document.querySelector("#sign_out").addEventListener("click", ()=>{
+document.querySelector("#sign_out").addEventListener("click", () => {
   signOut(auth).then(() => {
-    console.log( "// Sign-out successful.")
+    console.log("// Sign-out successful.")
     document.querySelector("#section-dashboard").classList.add("display_none")
     document.querySelector(".before-signIn").classList.remove("display_none")
-   }).catch((error) => {
-    console.log( "// An error happened.")
-   
-     // An error happened.
-   })
+  }).catch((error) => {
+    console.log("// An error happened.")
+
+    // An error happened.
+  })
 })
 
 // sign out end 
@@ -46,65 +51,66 @@ let addBlogBtn = document.querySelector("#addBlogBtn")
 console.log(blogTitle, blogDiscription, addBlogBtn)
 
 // dashboard functionality start
-let addBlog = () => {
-  // let obj ={
-  //     title: blogTitle.value,
-  //     discription: blogDiscription.value
-  // }
-  // try {
-  //     const docRef = await addDoc(collection(db, "obj"), obj);
-  //     console.log("Document written with ID: ", docRef.id);
-  //   } catch (e) {
-  //     console.error("Error adding document: ", e);
-  //   }
+let addBlog = async () => {
 
-  let carddiv = document.createElement("div")
-  carddiv.className = "card";
+  // add data in firestore db start
+  let blog = {
+    title: blogTitle.value.trim(),
+    discription: blogDiscription.value.trim()
+  }
+  try {
+    const docRef = await addDoc(collection(db, "blog"), blog);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+  // add data in firestore db end
 
-  let h1 = document.createElement("h2")
-  h1.className = "card-title";
-  h1.innerText = blogTitle.value;
+  // read data from firestore db start
+  fetchBlogs()
+  // read data from firestore db end
 
-  let cardPara = document.createElement("p")
-  cardPara.className = "card-discription"
-  cardPara.innerText = blogDiscription.value;
 
-  let cardDateDiv = document.createElement("div")
-  cardDateDiv.className = "card-date"
-  cardDateDiv.innerText = "14-August-1947"
-
-  carddiv.append(h1)
-  carddiv.append(cardPara)
-  carddiv.append(cardDateDiv)
-
-  document.querySelector("#blog-section").append(carddiv)
   blogTitle.value = ""
   blogDiscription.value = ""
+}
+addBlogBtn.addEventListener("click", addBlog)
 
 
+
+// the function that fetch data from fire db and show to the index page when page reload
+
+let fetchBlogs = async () => {
+
+  // read data from firestore db start
+  try {
+    const querySnapshot = await getDocs(collection(db, "blog"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().title}`);
+      blogSection.innerHTML +=
+        `<div class="card">
+        <h2 class="card-title">${doc.data().title}</h2>
+        <p class="card-discription">${doc.data().discription}</p>
+        <div class="card-date">14-August-1947</div>
+        <div class="more_options">
+          <p id="edit_blog_btn">Edit</p>
+          <i class="fa-solid fa-trash-can" id="del_blog_btn"></i>
+        </div>
+      </div>`
+
+    })
+  } catch (e) {
+    console.log(e)
+    blogSection.innerHTML = `<h1>Connection Error</h1>`
+  }
+  ;
+  // read data from firestore db end
 
 }
 
-addBlogBtn.addEventListener("click", addBlog)
-// dashboard functionality end
+fetchBlogs()
 
-// sign up authentication start
-// signupBtn.addEventListener("click", () => {
-//     console.log("working")
-//     // const auth = getAuth();
-//     createUserWithEmailAndPassword(auth, signupEmail.value, signupPass.value)
-//     console.log(signupEmail.value,signupPass.value)
-//     .then((userCredential) => {
-//         // Signed up
-//         const user = userCredential.user;
-//         console.log(user)
-//         // ...
-//     })
-//     .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         console.log(errorCode, errorMessage)
-//         // ..
-//     });
-// })
-// sign up authentication end
+// target a single blog
+blogSection.addEventListener(`click`,(e)=>{
+  console.log(e.target)
+})
--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore ,collection, addDoc,getDocs ,doc, deleteDoc} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,5 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export {app,auth}
+export {app,auth,getFirestore ,collection, addDoc, db, getDocs , doc, deleteDoc }
